5ae738b445ecd2ab077850cd6bda0d62e5a6a257
Yoverina Nurdin <yoverina_nurdin@yahoo.com>
2022-06-19 02:57:49 +0700

    destructuring set

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-// const monicaRestaurant = 
 
-const wendyRestaurant = new Set();
-wendyRestaurant.add("Tempura");
-wendyRestaurant.add("Bento");
-wendyRestaurant.add("Sushi");
-wendyRestaurant.add("Pancake");
-wendyRestaurant.add("Padang");
-wendyRestaurant.add("Katsu");
-wendyRestaurant.add("Geprek");
-wendyRestaurant.add("Pancake");
-wendyRestaurant.add("Eggy");
+const wendyRestaurant = new Set(["Tempura","Bento","Sushi","Pancake","Padang","Katsu","Geprek","Pancake","Eggy"]);
+const monicaRestaurant = new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]);
+const monicaColor = new Set(["Yellow", "Pink", "White", "Purple"]);
+const wendyColor = new Set(["Blue", "Black", "Grey"]);
 
 const firstUser = {
     name : "Monica",
     gender : "Female",
     age : 17,
     email : "monica@dingdong.com",
-    favoriteColor : new Set(["Yellow", "Pink", "White", "Purple"]),
+    favoriteColor : [...monicaColor],
     isHavePet : true,
     education : [{name: "SD 01", city: "Jakarta", graduate: 2016},{name: "SMP 02", city: "Jakarta", graduate: 2019}, {name: "SMA 03", city: "Tangerang"}],
-    favoriteRestaurant : new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"])
+    favoriteRestaurant : [...monicaRestaurant]
 };
 const secondUser = {
     name : "Wendy",
     gender : "Male",
     age : 23,
     email : "wendy@dingdong.com",
-    favoriteColor : new Set(["Blue", "Black", "Grey"]),
+    favoriteColor : [...wendyColor],
     isHavePet : false,
     education : [
         {name: "SD 02", city: "Jakarta", graduate: 2010},
         {name: "SMP 03", city: "Bogor", graduate: 2013}, 
         {name: "SMA 01", city: "Surabaya", graduate: 2016},
         {name: "Universitas Maju", city: "Tangerang"}],
-    favoriteRestaurant : wendyRestaurant
+    favoriteRestaurant : [...wendyRestaurant]
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
@@ -44,8 +37,11 @@ const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+    // console.log(users.length || users.size);
+    // console.log(users);
+    // console.log(typeof(users[0].favoriteRestaurant));
+    // console.log(typeof(users[1].favoriteRestaurant));
+    // console.log(users[0].favoriteRestaurant);
 }
 
 main();
